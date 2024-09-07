0e8a00aa911d5e9c4d6f94635db131d99e5cd0ca
Роман Куликов <rkulikov@bigland.ru>
2024-09-07 17:07:08 +0300

    wip stepik. Практика по работе с объектами 9.2 Шаг 13

---
+++ app/tasks/stepik/9/13.js
@@ -0,0 +1,41 @@
+let teams = [{
+    "name": "Team A", "players": [{
+        "name": "Player 1", "damage": 1500, "frags": 8, "zone": "Erangel"
+    }, {
+        "name": "Player 2", "damage": 1200, "frags": 5, "zone": "Miramar"
+    }, {
+        "name": "Player 3", "damage": 1800, "frags": 6, "zone": "Erangel"
+    }]
+}, {
+    "name": "Team B", "players": [{
+        "name": "Player 4", "damage": 1000, "frags": 4, "zone": "Miramar"
+    }, {
+        "name": "Player 5", "damage": 2000, "frags": 7, "zone": "Erangel"
+    }]
+}];
+
+
+let result = [];
+for (team of teams) {
+    let erangelDamage = 0;
+    let erangelFrags = 0;
+    let miramarDamage = 0;
+    let miramarFrags = 0;
+    let erangel;
+    let miramar;
+    for (player of team.players) {
+        if (player.zone === 'Erangel') {
+            erangelDamage += player.damage;
+            erangelFrags += player.frags;
+        }
+
+        if (player.zone === 'Miramar') {
+            miramarDamage += player.damage;
+            miramarFrags += player.frags;
+        }
+        erangel = erangelDamage / erangelFrags;
+        miramar = miramarDamage / miramarFrags;
+    }
+    result.push()
+    console.log(erangel, miramar)
+}
